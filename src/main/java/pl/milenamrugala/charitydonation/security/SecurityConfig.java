2cc3b32a9e4bf66fa20a95c5dd5eeffa6c742aa6
milenamrugala <milena.mrugala@icloud.com>
2023-05-19 14:37:16 +0200

    SecurityConfig updated

--- src/main/java/pl/milenamrugala/charitydonation/security/SecurityConfig.java
+++ src/main/java/pl/milenamrugala/charitydonation/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/registration", "/resources/**", "/").permitAll()
+                .antMatchers("/registration", "/resources/**", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
